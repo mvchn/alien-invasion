7a9f0886e3d70c82077c64bb285462a9b642fd68
mvchn <movchan@gmail.com>
2021-12-13 15:59:53 +0200

    add alien rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -69,18 +69,29 @@ class AlienInvasion:
 		# create aliens and calculate distanse between aliens 
 		# distance between aliens is width of the alien
 		alien = Alien(self)
-		alien_width = alien.rect.width
+		alien_width, alien_height = alien.rect.size
 		available_space_x = self.settings.screen_width - (2 * alien_width)
 		number_aliens_x = available_space_x // (2 * alien_width) 
 
-		# create aliens first row
-		for alien_number in range(number_aliens_x):
-			# create alien and set to row
-			alien = Alien(self)
-			alien.x = alien_width + 2 * alien_width * alien_number
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+		# check rows 
+		ship_height = self.ship.rect.height
+		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+		number_rows = available_space_y // (2 * alien_height)
 
+		# create aliens first row
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens_x):
+				self._create_alien(alien_number, row_number)
+			
+
+	def _create_alien(self, alien_number, row_number):
+		""" Create alien and set to row """
+		alien = Alien(self)
+		alien_width = alien.rect.width
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+		self.aliens.add(alien)
 
 	def _fire_bullet(self):
 		""" Create bullet and add it ti bullets group """
